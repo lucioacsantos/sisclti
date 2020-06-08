a618da806ebaaacf99aa65d3de2054c36d38c674
Lúcio A C Santos <lucio.alexandre@marinha.mil.br>
2020-06-08 11:34:45 -0300

    Instruções de instalação

---
+++ loading.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+###
+#
+#Author: Lucio Alexandre Correia dos Santos 99.2429.91
+#Title: Loading - loading.sh
+#
+##
+
+#Verifica execução com usuário administrador
+if [[ $EUID -ne 0 ]]; then
+
+	whiptail --title "ERRO" --msgbox "Este script deve ser executado como root. 
+		Cancelando a instalação. Clique em OK para continuar." 8 78
+	exit 1
+fi
+
+clear
+echo "";
+echo " XXXX";
+echo "X";
+echo " XXXX";
+echo "     X";
+echo "XXXX";
+echo "";
+echo "Aguarde.";
+sleep 0.2
+clear
+echo "";
+echo " XXXX  XXXXX";
+echo "X        X";
+echo " XXXX    X";
+echo "     X   X";
+echo "XXXX   XXXXX";
+echo "";
+echo "Aguarde....";
+sleep 0.2
+clear
+echo "";
+echo " XXXX  XXXXX   XXXX";
+echo "X        X    X";
+echo " XXXX    X     XXXX";
+echo "     X   X         X";
+echo "XXXX   XXXXX   XXXX";
+echo "";
+echo "Aguarde....";
+sleep 0.2
+clear
+echo "";
+echo " XXXXX";
+echo "X";
+echo "X";
+echo "X";
+echo " XXXXX";
+echo "";
+echo "Aguarde.";
+sleep 0.2
+clear
+echo "";
+echo " XXXXX  X";
+echo "X       X";
+echo "X       X";
+echo "X       X";
+echo " XXXXX  XXXXX";
+echo "";
+echo "Aguarde..";
+sleep 0.2
+clear
+echo "";
+echo " XXXXX  X     XXXXX";
+echo "X       X       X";
+echo "X       X       X";
+echo "X       X       X";
+echo " XXXXX  XXXXX   X";
+echo "";
+echo "Aguarde...";
+sleep 0.2
+clear
+echo "";
+echo " XXXXX  X     XXXXX  XXXXX";
+echo "X       X       X      X";
+echo "X       X       X      X";
+echo "X       X       X      X";
+echo " XXXXX  XXXXX   X    XXXXX";
+echo "";
+echo "Aguarde....";
+sleep 0.2
+clear
+echo "";
+echo " XXXXX  X     XXXXX  XXXXX";
+echo "X       X       X      X";
+echo "X       X       X      X";
+echo "X       X       X      X";
+echo " XXXXX  XXXXX   X    XXXXX";
+echo "";
+echo "Aguarde.";
+sleep 0.2
+clear
+echo "";
+echo " XXXX   XXXXX   XXXX    XXXXX  X     XXXXX  XXXXX";
+echo "X         X    X       X       X       X      X";
+echo " XXXX     X     XXXX   X       X       X      X";
+echo "     X    X         X  X       X       X      X";
+echo " XXXX   XXXXX   XXXX    XXXXX  XXXXX   X    XXXXX";
+echo "";
+echo "Aguarde....";
+sleep 0.5
---
+++ sisclti.sh
@@ -0,0 +1,147 @@
+#/bin/bash
+#!/bin/bash
+
+###
+#
+#Author: Lucio Alexandre Correia dos Santos 99.2429.91
+#Title: SisCLTI - sisclti.sh
+#
+##
+
+#Verifica execução com usuário administrador(root)
+if [[ $EUID -ne 0 ]]; then
+
+	whiptail --title "ERRO" --msgbox "Este script deve ser executado como root. 
+		Cancelando a instalação. Clique em OK para continuar." 8 78
+	exit 1
+fi
+
+#Executa o loading
+bash loading.sh
+
+#Solicita continuar com a instalação
+if whiptail \
+   --title 'SISCLTI' \
+   --yesno '\n     
+CONFIGURAÇÃO DO SERVIDOR WEB COM POSTGRESQL\n\n
+             DESEJA CONTINUAR?' 0 0
+
+#Continuando a configuração do servidor web
+then
+
+#Instalação dos pacotes necessários
+yum group install -y "Servidor de Web Básico"
+yum -y install postgresql-server
+yum -y install php php-mbstring php-gd php-dom php-pgsql
+
+#Inicializando e habilitando os serviços
+systemctl start httpd
+postgresql-setup initdb
+systemctl start postgresql
+systemctl enable httpd
+systemctl enable postgresql
+
+#Configurando PSQL
+curl http://clti.com3dn.mb/repodata/pg_hba.conf > /var/lib/pgsql/data/pg_hba.conf
+systemctl restart postgresql
+
+#Configurando o firewall e o SELinux
+firewall-cmd --add-service=http
+firewall-cmd --add-service=http --permanent
+firewall-cmd --add-port=443/tcp
+firewall-cmd --add-port=443/tcp --permanent
+setsebool -P httpd_can_network_connect 1
+setsebool -P httpd_can_network_connect_db 1
+
+#Solicita nome de usuário para banco de dados
+while [ true ]
+do
+	BDUSR=$(whiptail --title "Usuário do Banco de Dados" --inputbox "Informe um usuário para o banco de dados do SisCLTI" 10 60  3>&1 1>&2 2>&3)
+	exitstatus=$?
+	if [ $exitstatus = 1 ]; then
+		echo "Cancelado."
+		exit
+	fi
+	BDUSR=$(echo "$BDUSR" | sed 's/ //g')
+	BDUSR=$(echo $BDUSR | tr '[:upper:]' '[:lower:]')
+	if [ "${#BDUSR}" -ge 5 ]; then 
+		if (whiptail --title "Confirmação do Usuário do Banco de Dados" --yesno "Usuário do Banco de Dados: "$BDUSR 8 78) then
+    		break	
+		fi
+	else
+		whiptail --title "ERRO" --msgbox "Usuário do Banco de Dados inválido. Clique em OK para continuar." 8 78
+	fi
+
+done
+
+#Solicita senha para o usuário criado do banco de dados para o SisCLTI
+while [ true ]
+do
+	BDPWS=$(whiptail --title "Senha para o Usuário Criado" --inputbox "Informe a senha para o usuário criado para o SisCLTI" 10 60  3>&1 1>&2 2>&3)
+	exitstatus=$?
+	if [ $exitstatus = 1 ]; then
+		echo "Cancelado."
+		exit
+	fi
+	BDPWS=$(echo "$BDPWS" | sed 's/ //g')
+	BDPWS=$(echo $BDPWS | tr '[:upper:]' '[:lower:]')
+	if [ "${#BDPWS}" -ge 5 ]; then 
+		if (whiptail --title "Confirmação da Senha" --yesno "Senha do Usuário: "$BDPWS 8 78) then
+    		break	
+		fi
+	else
+		whiptail --title "ERRO" --msgbox "Senha do Usuário inválida. Clique em OK para continuar." 8 78
+	fi
+
+done
+
+#Solicita URL/IP do SisCLTI
+while [ true ]
+do
+	URLIP=$(whiptail --title "URL/IP do Servidor" --inputbox "Informe a URL ou End. IP do SisCLTI (SEM HTTP://)" 10 60  3>&1 1>&2 2>&3)
+	exitstatus=$?
+	if [ $exitstatus = 1 ]; then
+		echo "Cancelado."
+		exit
+	fi
+	URLIP=$(echo "$URLIP" | sed 's/ //g')
+	URLIP=$(echo $URLIP | tr '[:upper:]' '[:lower:]')
+	if [ "${#BDPWS}" -ge 5 ]; then 
+		if (whiptail --title "Confirmação da URL/IP" --yesno "URL/IP: "$URLIP 8 78) then
+    		break	
+		fi
+	else
+		whiptail --title "ERRO" --msgbox "URL/IP inválida. Clique em OK para continuar." 8 78
+	fi
+
+done
+
+#Executando configuração do banco de dados PostgreSQL
+psql -c "CREATE ROLE $BDUSR" -U postgres
+psql -c "ALTER ROLE $BDUSR WITH SUPERUSER INHERIT NOCREATEROLE CREATEDB LOGIN PASSWORD '$BDPWS'" -U postgres
+psql -c "CREATE DATABASE db_clti WITH TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='pt_BR.UTF-8' LC_CTYPE='pt_BR.UTF-8'" -U postgres
+psql -c "ALTER DATABASE db_clti OWNER TO $BDUSR"
+psql -c "UPDATE db_clti.tb_config SET valor='http://$URLIP/sisclti' WHERE parametro='URL' "
+
+#Copia SisCLTI
+cp -r $PWD/ /var/www/html/
+
+#Configurações inciciais do sistema
+sed -i "s/sisclti/$BDPWS/g" /var/www/html/sisclti/class/config.php
+
+#Encerrando instalação
+if whiptail \
+   --title 'CLTI-3°DN' \
+   --no-button 'Atualização' --yes-button 'Nova' --yesno '\n     
+ACESSE PELO NAVEGADOR COM URL/sisclti \n\n 
+	Usuário 12345678/Senha mb@2020# \n\n
+    	DESEJA ENCERRAR?' 0 0
+
+fi
+exti 0
+
+#Interrompendo a configuração caso selecione não no início
+else
+	exit
+fi
+
