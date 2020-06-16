92b2a99d586c959d343f61b457236c9c39be4079
Lucio A C Santos <lucioacsantos@gmail.com>
2020-06-15 21:49:15 -0300

    Alterações Tabela memórias/Tabela estações

--- update.php
+++ update.php
@@ -9,6 +9,27 @@ $pg = new PgSql();
 
 echo "Executando atualização...";
 
+$sql = "CREATE TABLE db_clti.tb_memorias (
+	idtb_memorias serial NOT NULL,
+	tipo varchar(255) NOT NULL,
+	modelo varchar(255) NOT NULL,
+	clock int4 NOT NULL,
+	CONSTRAINT tb_memorias_pkey PRIMARY KEY (idtb_memorias)
+);";
+$pg->exec($sql);
+
+$sql = "INSERT INTO db_clti.tb_memorias(tipo,modelo,clock) values ('DDR', 'PC-1600',	200),
+('DDR', 'PC-2100',	266),('DDR', 'PC-2400', 300),('DDR', 'PC-2700', 333),('DDR', 'PC-3000', 370),
+('DDR', 'PC-3200', 400),('DDR', 'PC-3700', 466),('DDR', 'PC-4000', 500),('DDR2', 'PC2-4200', 533),
+('DDR2', 'PC2-5300', 667),('DDR2', 'PC2-6400', 800),('DDR2', 'PC2-7400', 933),('DDR2', 'PC2-8500', 1066),
+('DDR2', 'PC2-9600', 1200),('DDR2', 'PC2-1066', 1333),('DDR3', 'PC3-6400', 800),('DDR3', 'PC3-8500', 1066),
+('DDR3', 'PC3-10600', 1333),('DDR3', 'PC3-12800', 1600),('DDR3', 'PC3-14900', 1866),('DDR3', 'PC3-16000', 2000),
+('DDR3', 'PC3-17000', 2133),('DDR3', 'PC3-19200', 2400),('DDR3', 'PC3-20800', 2600),('DDR3', 'PC3-22400', 2800),
+('DDR4', 'PC4-12800', 1600),('DDR4', 'PC4-14900', 1866),('DDR4', 'PC4-17000', 2133),('DDR4', 'PC4-19200', 2400),
+('DDR4', 'PC4-21300', 2666),('DDR4', 'PC4-25600', 3200),('DDR4', 'PC4-27700', 3466),('DDR4', 'PC4-28000', 3600),
+('DDR4', 'PC4-32000', 4000);";
+$pg->exec($sql);
+
 $sql = "ALTER TABLE db_clti.tb_estacoes ADD idtb_memorias int4 NULL";
 $pg->exec($sql);
 
@@ -29,27 +50,6 @@ $pg->exec($sql);
 
 $sql = "ALTER TABLE db_clti.tb_estacoes ADD CONSTRAINT tb_estacoes_fk_3 FOREIGN KEY (idtb_sor) 
     REFERENCES db_clti.tb_sor(idtb_sor)";
-$pg->exec($sql);
-
-$sql = "CREATE TABLE db_clti.tb_memorias (
-	idtb_memorias serial NOT NULL,
-	tipo varchar(255) NOT NULL,
-	modelo varchar(255) NOT NULL,
-	clock int4 NOT NULL,
-	CONSTRAINT tb_memorias_pkey PRIMARY KEY (idtb_memorias)
-);";
-$pg->exec($sql);
-
-$sql = "INSERT INTO db_clti.tb_memorias(tipo,modelo,clock) values ('DDR', 'PC-1600',	200),
-('DDR', 'PC-2100',	266),('DDR', 'PC-2400', 300),('DDR', 'PC-2700', 333),('DDR', 'PC-3000', 370),
-('DDR', 'PC-3200', 400),('DDR', 'PC-3700', 466),('DDR', 'PC-4000', 500),('DDR2', 'PC2-4200', 533),
-('DDR2', 'PC2-5300', 667),('DDR2', 'PC2-6400', 800),('DDR2', 'PC2-7400', 933),('DDR2', 'PC2-8500', 1066),
-('DDR2', 'PC2-9600', 1200),('DDR2', 'PC2-1066', 1333),('DDR3', 'PC3-6400', 800),('DDR3', 'PC3-8500', 1066),
-('DDR3', 'PC3-10600', 1333),('DDR3', 'PC3-12800', 1600),('DDR3', 'PC3-14900', 1866),('DDR3', 'PC3-16000', 2000),
-('DDR3', 'PC3-17000', 2133),('DDR3', 'PC3-19200', 2400),('DDR3', 'PC3-20800', 2600),('DDR3', 'PC3-22400', 2800),
-('DDR4', 'PC4-12800', 1600),('DDR4', 'PC4-14900', 1866),('DDR4', 'PC4-17000', 2133),('DDR4', 'PC4-19200', 2400),
-('DDR4', 'PC4-21300', 2666),('DDR4', 'PC4-25600', 3200),('DDR4', 'PC4-27700', 3466),('DDR4', 'PC4-28000', 3600),
-('DDR4', 'PC4-32000', 4000);";
 $pg->exec($sql);
 
 $sql = "ALTER TABLE db_clti.tb_estacoes ALTER COLUMN idtb_memorias NOT NULL";
