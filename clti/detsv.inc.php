52624190e9de90b02d8c2ed91b2109aae1acd38f
Lucio A C Santos <lucioacsantos@gmail.com>
2022-05-15 21:36:00 -0300

    Submódulo detalhe de serviço

--- clti/detsv.inc.php
+++ clti/detsv.inc.php
@@ -31,7 +31,10 @@ $pesclti = new PessoalCLTI();
 $relsv = new RelServico();
 
 /* Recupera informações */
+$pess_clti->ordena = 'ORDER BY data_entra_servico ASC';
 $row = $pesclti->SelectALL();
+$pess_clti->ordena = 'ORDER BY idtb_posto_grad ASC';
+$svc_entra = $pess_clti->SelectEscalaSV();
 
 /* Carrega form para Novo Relatório */
 if ($act == 'cad') {
@@ -42,7 +45,7 @@ if ($act == 'cad') {
         $servico = $relsv->SelectIdDetSv();
     }
     else{
-        $relatorio = (object)['idtb_det_serv'=>'','sup_servico'=>'','data_entra_servico'=>'',
+        $servico = (object)['idtb_det_serv'=>'','sup_servico'=>'','data_entra_servico'=>'',
             'data_sai_servico'=>'','status'=>'PROGRAMADO'];
     }
     
@@ -53,21 +56,21 @@ if ($act == 'cad') {
                 <div id=\"form-cadastro\">
                     <form id=\"insertom\" action=\"?cmd=relservico&act=insert\" method=\"post\" enctype=\"multipart/form-data\">
                         <fieldset>
-                            <legend>Relatório de Serviço Nº $num_rel - Supervisor de Serviço: $svc_sai->sigla_posto_grad $svc_sai->nome_guerra</legend>
+                            <legend>Detalhe de Serviço</legend>
                             <div class=\"form-group\">
                                 <label for=\"data_entra_servico\">Serviço do dia:</label>
                                 <input id=\"data_entra_servico\" class=\"form-control\" name=\"data_entra_servico\" type=\"date\" 
-                                    value=\"$relatorio->data_entra_servico\">
+                                    value=\"$servico->data_entra_servico\">
                             </div>
                             <div class=\"form-group\">
                                 <label for=\"data_sai_servico\">Para o dia:</label>
                                 <input id=\"data_sai_servico\" class=\"form-control\" name=\"data_sai_servico\" type=\"date\"
-                                    value=\"$relatorio->data_sai_servico\">
+                                    value=\"$servico->data_sai_servico\">
                             </div>
                             <div class=\"form-group\">
                                 <label for=\"sup_entra_servico\">Supervisor que entra:</label>
                                 <select id=\"sup_entra_servico\" class=\"form-control\" name=\"sup_entra_servico\">
-                                        <option value=\"$relatorio->sup_entra_servico\" selected>
+                                        <option value=\"$servico->sup_entra_servico\" selected>
                                             ".$svc_atual->sigla_posto_grad." - ".$svc_atual->nome_guerra."</option>";
                                             foreach ($svc_entra as $key => $value) {
                                                 echo"<option value=\"".$value->idtb_lotacao_clti."\">
@@ -98,7 +101,7 @@ if ($act == 'cad') {
                             </div>
                         </fieldset>
                         <input id=\"sup_sai_servico\" name=\"sup_sai_servico\" value=\"$svc_sai->idtb_lotacao_clti\" type=\"hidden\">
-                        <input id=\"idtb_rel_servico\" name=\"idtb_rel_servico\" value=\"$relatorio->idtb_rel_servico\" type=\"hidden\">
+                        <input id=\"idtb_rel_servico\" name=\"idtb_rel_servico\" value=\"$servico->idtb_rel_servico\" type=\"hidden\">
                         <input id=\"num_rel\" name=\"num_rel\" value=\"$num_rel\" type=\"hidden\">
                         <input id=\"status\" name=\"status\" value=\"Em andamento\" type=\"hidden\">
                         <input class=\"btn btn-primary btn-block\" type=\"submit\" value=\"Salvar\">
